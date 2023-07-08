e7f86a07a9198756a476faf149cb79e6cd5877fb
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-09 01:54:03 +0500

    Delete Task 3 directory

--- Task 3/index.js
+++
@@ -1,3 +0,0 @@
-var Poetname = "Albert Einstein once said,";
-var Quote = '"A person who never made a mistake never tried anything new."';
-console.log(Poetname + Quote);
--- Task 3/index.ts
+++
@@ -1,4 +0,0 @@
-let Poetname = "Albert Einstein once said,";
-let Quote = '"A person who never made a mistake never tried anything new."';
-
-console.log(Poetname + Quote);
