295ea6590c4098457e8b6a0421e3ab7325741403
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-09 11:10:34 +0500

    Delete Task5 directory

--- Task5/index.js
+++
@@ -1,4 +0,0 @@
-var personNameWithWhitespace = "\t  John Doe  \n e \n";
-console.log("Name with whitespace: ", personNameWithWhitespace);
-var strippedName = personNameWithWhitespace.trim();
-console.log("Stripped name: ", strippedName);
--- Task5/index.ts
+++
@@ -1,5 +0,0 @@
-let personNameWithWhitespace: string = "\t  HuzuArif  \n";
-console.log("Name with whitespace: ", personNameWithWhitespace);
-
-let strippedName: string = personNameWithWhitespace.trim();
-console.log("Stripped name: ", strippedName);
