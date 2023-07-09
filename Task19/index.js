89454ddac5bdc1c6b6fa11386362a2cc493f5212
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-10 01:18:28 +0500

    Add files via upload

---
+++ Task19/index.js
@@ -0,0 +1,13 @@
+var landmarks = [
+    "Eiffel Tower",
+    "Great Wall of China",
+    "Taj Mahal",
+    "Machu Picchu",
+    "Pyramids of Giza"
+];
+console.log("List of Famous Landmarks:", landmarks);
+var landmark = {
+    landmark: "Eiffel Tower",
+    Countries: "Paris"
+};
+console.log(landmark);
---
+++ Task19/index.ts
@@ -0,0 +1,15 @@
+const landmarks = [
+    "Eiffel Tower",
+    "Great Wall of China",
+    "Taj Mahal",
+    "Machu Picchu",
+    "Pyramids of Giza"
+  ];
+  
+  console.log("List of Famous Landmarks:" , landmarks);
+
+const  landmark : {landmark : string , Countries : string} ={
+    landmark: "Eiffel Tower" ,
+    Countries: "Paris"
+
+}; console.log(landmark);
