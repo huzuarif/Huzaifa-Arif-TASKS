8a3a7da1a9eb291997da9445ca136cc6382ece5c
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-10 01:23:48 +0500

    Add files via upload

---
+++ Task20/index.js
@@ -0,0 +1,5 @@
+var numbers = [1, 2, 3, 4, 5];
+// Intentional error: Accessing an index that is out of bounds
+console.log(numbers[10]); // Trying to access index 10 which doesn't exist
+// Corrected version:
+// console.log(numbers[2]); // Accessing a valid index
---
+++ Task20/index.ts
@@ -0,0 +1,9 @@
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+// Intentional error: Accessing an index that is out of bounds
+console.log(numbers[10]); // Trying to access index 10 which doesn't exist
+
+// Corrected version:
+// console.log(numbers[2]); // Accessing a valid index
+
+
