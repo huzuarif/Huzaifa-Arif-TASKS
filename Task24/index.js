9fec4f208e4f9b4b5050c9d5c309b6c12bf65a62
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-10 02:35:43 +0500

    Add files via upload

---
+++ Task24/index.js
@@ -0,0 +1,7 @@
+var alien_color = 'red';
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the alien.");
+}
+else if (alien_color === 'red') {
+    console.log("Congratulations! You just earned 10 points.");
+}
