9d1df30955844302647a3d18b56bb13e461be5b5
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-09 11:24:54 +0500

    Add files via upload

---
+++ Task12/index.js
@@ -0,0 +1,4 @@
+var guestList = ["Maleeha", "Albash", "Haider", "usman", "Haseeb bhai"];
+for (var i = 0; i < guestList.length; i++) {
+    console.log("Dear " + guestList[i] + ",\nYou are cordially invited to dinner at my place. It would be an honor to have you join us.\nPlease let me know if you can attend.\n\nBest regards,\n[Your Name]");
+}
---
+++ Task12/index.ts
@@ -0,0 +1,4 @@
+let guestList=["Maleeha" , "Albash" , "Haider" , "usman" , "Haseeb bhai"]
+for (let i = 0; i < guestList.length; i++) {
+  console.log("Dear " + guestList[i] + ",\nYou are cordially invited to dinner at my place. It would be an honor to have you join us.\nPlease let me know if you can attend.\n\nBest regards,\n[Malik Huzaifa Arif]");
+}
