951404fbf78fa23d048b1022c0f9d2722a1e9ed3
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-09 10:40:43 +0500

    Add files via upload

---
+++ Task10/index.js
@@ -0,0 +1,13 @@
+var personname1 = ["malik", "huzaifa", "arif"];
+console.log(personname1[0] + " is a  caste");
+console.log(personname1[1] + " is my name");
+console.log(personname1[2] + " is my father's name");
+// or when your have a large array so this is very difficult to write or intialize index one by one,this maye take very
+// long time so in short we declare all my index in array
+console.log("\n");
+var personname2 = ["malik", "huzaifa", "arif"];
+for (var i = 0; i < personname2.length; i++) {
+    console.log(personname1[0] + " is a  caste");
+    console.log(personname1[1] + " is my name");
+    console.log(personname1[2] + " is my father's name");
+}
---
+++ Task10/index.ts
@@ -0,0 +1,14 @@
+let personname1=["malik" , "huzaifa" , "arif"];
+console.log(personname1[0] + " is a  caste");
+console.log(personname1[1] + " is my name");
+console.log(personname1[2] + " is my father's name");
+
+// Alternatively, you can use a loop to iterate over the array
+
+let personname2=["malik" , "huzaifa" , "arif"];
+for(let i=0 ; i<personname2.length ; i++){
+    console.log(personname1[0] + " is a  caste");
+    console.log(personname1[1] + " is my name");
+    console.log(personname1[2] + " is my father's name");
+}
+
