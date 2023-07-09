fe823b41747f4bdec2b9520204968a94d2736832
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-09 11:05:15 +0500

    Add files via upload

---
+++ Task11/index.js
@@ -0,0 +1,5 @@
+var motorcycles = ["Honda", "Yamaha", "Kawasaki", "Suzuki", "Ducati"];
+// Print statements about each motorcycle
+for (var i = 0; i < motorcycles.length; i++) {
+    console.log("I would like to own a " + motorcycles[i] + " motorcycle.");
+}
---
+++ Task11/index.ts
@@ -0,0 +1,6 @@
+let motorcycles: string[] = ["Honda", "Yamaha", "Kawasaki", "Suzuki", "Ducati"];
+
+// Print statements about each motorcycle
+for (let i = 0; i < motorcycles.length; i++) {
+  console.log("I would like to own a " + motorcycles[i] + " motorcycle.");
+}
