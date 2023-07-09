70d6728f736dc3638d5d06724027e994c002e3f5
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-09 11:10:17 +0500

    Delete Task4 directory

--- Task4/index.js
+++
@@ -1,3 +0,0 @@
-var famous_person = "Albert Einstein";
-var composemessage = famous_person + ' once said, "Stay hungry, stay foolish."';
-console.log(composemessage);
--- Task4/index.ts
+++
@@ -1,3 +0,0 @@
-let famous_person = "Quaid e Azam";
-let composemessage = famous_person + 'work , work , work you will be successful"' ; 
-console.log(composemessage);
