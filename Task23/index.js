ab42b0a879bda70332d18d884934bdf2c2bfdefa
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-10 02:14:56 +0500

    Add files via upload

---
+++ Task23/index.js
@@ -0,0 +1,10 @@
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
+
+
+var alien_colorr = 'red';
+if (alien_colorr === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
---
+++ Task23/index.ts
@@ -0,0 +1,16 @@
+// Now, here is a part 1 where the if statement fails and there is no output:
+
+let alien_color = 'green';
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points.");
+}
+
+
+// Now, here is a part 2 where the if statement fails and there is no output:
+
+let alien_colorr = 'red';
+
+if (alien_colorr === 'green') {
+  console.log("Congratulations! You just earned 5 points.");
+}
