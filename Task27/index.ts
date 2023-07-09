73eff7ce932f1fcc0de4af6237c1bb20fa375db7
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-10 02:49:13 +0500

    Add files via upload

---
+++ Task27/index.ts
@@ -0,0 +1,21 @@
+let favorite_fruits: string[] = ['apple', 'banana', 'mango'];
+
+if (favorite_fruits.indexOf('apple') !== -1) {
+  console.log("You really like apples!");
+}
+
+if (favorite_fruits.indexOf('banana') !== -1) {
+  console.log("You really like bananas!");
+}
+
+if (favorite_fruits.indexOf('mango') !== -1) {
+  console.log("You really like mangoes!");
+}
+
+if (favorite_fruits.indexOf('strawberry') !== -1) {
+  console.log("You really like strawberries!");
+}
+
+if (favorite_fruits.indexOf('orange') !== -1) {
+  console.log("You really like oranges!");
+}
