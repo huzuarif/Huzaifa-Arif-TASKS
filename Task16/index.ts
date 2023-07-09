8c2cbc1dc81afcf1129d7e78019a8bc48dd50977
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-10 00:19:36 +0500

    Add files via upload

---
+++ Task16/index.ts
@@ -0,0 +1,45 @@
+// Seeing the World: Think of at least five places in the world you’d like to visit.
+// • Store the locations in a array. Make sure the array is not in alphabetical order.
+
+let Country_places = ["Canada" , "America" , "Makka" , "Iraq" , "Madina"];
+
+// • Print your array in its original order.
+
+console.log("Original Order:" ,Country_places);
+
+// • Print your array in alphabetical order without modifying the actual list.
+
+console.log("\nAlphabetical Order:",Country_places.sort());
+
+// • Show that your array is still in its original order by printing it.
+
+console.log("\nStill in Original Order:" ,Country_places);
+
+// • Print your array in reverse alphabetical order without changing the order of the original list.
+
+console.log("\nReverse Alphabetical Order:" ,Country_places.reverse());
+
+// • Show that your array is still in its original order by printing it again.
+
+console.log("\nStill in Original Order Again:" ,Country_places);
+
+// • Reverse the order of your list. Print the array to show that its order has changed.
+
+console.log("\nits order has changed " ,Country_places.reverse());
+
+// • Reverse the order of your list again. Print the list to show it’s back to its original order.
+
+console.log("\nits back to its original order" ,Country_places.reverse());
+
+// • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+
+console.log("\nits order has been changed." ,Country_places.sort());
+
+// • Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+
+console.log("\nits order has changed." ,Country_places.sort().reverse());
+
+
+
+
+
