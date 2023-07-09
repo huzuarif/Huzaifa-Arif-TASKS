5f21cd37833cbd77f4d0f95bd9ef3e1785f458d9
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-09 11:08:46 +0500

    Delete Task1 directory

--- Task1/index.js
+++
@@ -1,2 +0,0 @@
-var Personname = "Huzaifa";
-console.log(Personname + " Hello Eric, would you like to learn some Python today?");
--- Task1/index.ts
+++
@@ -1,2 +0,0 @@
-let Personname="Eric";
-console.log(" Hello " + Personname  + " Eric, would you like to learn some Python today?");
