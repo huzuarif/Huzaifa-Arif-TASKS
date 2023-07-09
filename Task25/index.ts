575d20ce2693f4f4ca7d22cfb1eeb7a16773ffe7
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-10 02:38:31 +0500

    Add files via upload

---
+++ Task25/index.ts
@@ -0,0 +1,11 @@
+let alien_color = 'green';
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points.");
+} else if (alien_color === 'yellow') {
+  console.log("Congratulations! You just earned 10 points.");
+} else if (alien_color === 'red') {
+  console.log("Congratulations! You just earned 15 points.");
+} else {
+  console.log("Invalid alien color.");
+}
