fd7e0f304a594ee5628a7f8d325d71538d620f00
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-11 01:55:23 +0500

    Add files via upload

---
+++ Task38/index.ts
@@ -0,0 +1,10 @@
+function show_magicians(magicians: string[]): void {
+    for (let i = 0; i < magicians.length; i++) {
+      console.log(magicians[i]);
+    }
+  }
+  
+  let magicians: string[] = ['Ali', 'Hamza', 'Moeen', 'Adnan'];
+  
+  show_magicians(magicians);
+  
