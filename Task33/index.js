f240ddc2b20d3f7dbaf3478abd1472dbc2a23882
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-11 00:19:45 +0500

    Add files via upload

---
+++ Task33/index.js
@@ -0,0 +1,4 @@
+function make_shirt(size, message) {
+    console.log("The shirt size is ".concat(size, " and it will have the message \"").concat(message, "\" printed on it."));
+}
+make_shirt('M', 'I love coding'); // Calling the function with size 'M' and message 'I love coding'
---
+++ Task33/index.ts
@@ -0,0 +1,6 @@
+function make_shirt(size: string, message: string) {
+  console.log(`The shirt size is ${size} and it will have the message "${message}" printed on it.`);
+}
+
+make_shirt('M', 'I love coding'); // Calling the function with size 'M' and message 'I love coding'
+ 
