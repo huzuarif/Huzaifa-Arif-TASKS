b86109597bdb9e8bdac0f0c36ed1a2dc90300edd
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-10 23:33:18 +0500

    Add files via upload

---
+++ Task30/index.js
@@ -0,0 +1,20 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+
+    if (number === 1) {
+        console.log(number, "st");
+    }
+    else if (number === 2) {
+        console.log(number, "nd");
+    }
+    else if (number === 3) {
+        console.log(number, "rd");
+    }
+    else if (number === 4) {
+        console.log(number, "th");
+    }
+    else {
+        console.log(number, "th");
+    }
+}
