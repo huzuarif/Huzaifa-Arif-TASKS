ccf1651f42fc785b891e7cec8665a32ea9055180
huzuarif <137005553+huzuarif@users.noreply.github.com>
2023-07-11 00:54:03 +0500

    Add files via upload

---
+++ Task35/index.js
@@ -0,0 +1,7 @@
+function describe_city(city, country) {
+    if (country === void 0) { country = "Pakistan"; }
+    console.log(city, "is in", country);
+}
+describe_city("karachi");
+describe_city("islamabad");
+describe_city("rawalpindi");
